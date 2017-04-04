dd59cdb5bbf26579e938354c8ae943660081008c
zemr <rzedzicka@gmail.com>
2017-04-04 15:13:08 +0200

    simplify hexagon 6p

--- src/figures/hexagon-6p.js
+++ src/figures/hexagon-6p.js
@@ -1,24 +1,54 @@
 import React from 'react';
 
+const radius = 200, pointPI = (2 * Math.PI / 6);
+let i;
+
 class Hexagon extends React.Component {
   componentDidMount() {
     this.draw();
-    this.compute();
+  }
+
+  drawNegative(context, color) {
+    context.fillStyle = color;
+
+    for (i = 0; i < 6; i += 2) {
+      context.beginPath();
+      context.moveTo(radius + radius*Math.cos(i * pointPI), radius + radius*Math.sin(i * pointPI));
+      context.lineTo(radius + radius*Math.cos((i+1) * pointPI), radius + radius*Math.sin((i+1) * pointPI));
+      context.lineTo(radius, radius);
+      context.fill();
+    }
+  }
+
+  drawPositive(context, color) {
+    context.fillStyle = color;
+
+    for (i = 1; i < 6; i += 2) {
+      context.beginPath();
+      context.moveTo(radius + radius*Math.cos(i * pointPI), radius + radius*Math.sin(i * pointPI));
+      context.lineTo(radius + radius*Math.cos((i+1) * pointPI), radius + radius*Math.sin((i+1) * pointPI));
+      context.lineTo(radius, radius);
+      context.fill();
+    }
+  }
+
+  draw() {
+    const context = document.getElementById('canvas').getContext('2d');
+    this.drawNegative(context, 'olivedrab');
+    this.drawPositive(context, 'orange');
   }
 
   compute() {
-    const center = 200, radius = 200, pointPI = (2 * Math.PI / 6);
-
     /* hexagon width */
     console.log('width',
       Math.sqrt(
         Math.pow(
-          (center + radius * Math.cos(0)) - (center + radius * Math.cos(3 * pointPI)),
+          (radius + radius * Math.cos(0)) - (radius + radius * Math.cos(3 * pointPI)),
           2
         )
         +
         Math.pow(
-          (center + radius * Math.sin(0)) - (center + radius * Math.sin(3 * pointPI)),
+          (radius + radius * Math.sin(0)) - (radius + radius * Math.sin(3 * pointPI)),
           2
         )
       )
@@ -27,67 +57,22 @@ class Hexagon extends React.Component {
     console.log('height',
       Math.sqrt(
         Math.pow(
-          (center + radius * Math.cos(2 * pointPI)) - (center + radius * Math.cos(4 * pointPI)),
+          (radius + radius * Math.cos(2 * pointPI)) - (radius + radius * Math.cos(4 * pointPI)),
           2
         )
         +
         Math.pow(
-          (center + radius * Math.sin(2 * pointPI)) - (center + radius * Math.sin(4 * pointPI)),
+          (radius + radius * Math.sin(2 * pointPI)) - (radius + radius * Math.sin(4 * pointPI)),
           2
         )
       )
     );
   }
 
-  draw() {
-    const context = this.refs.canvas.getContext('2d');
-    const center = 200, radius = 200, pointPI = (2 * Math.PI / 6);
-
-    context.fillStyle = 'olivedrab';
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(0), center + radius*Math.sin(0));
-    context.lineTo(center + radius*Math.cos(pointPI), center + radius*Math.sin(pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(2*pointPI), center + radius*Math.sin(2*pointPI));
-    context.lineTo(center + radius*Math.cos(3*pointPI), center + radius*Math.sin(3*pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(4*pointPI), center + radius*Math.sin(4*pointPI));
-    context.lineTo(center + radius*Math.cos(5*pointPI), center + radius*Math.sin(5*pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-
-    context.fillStyle = 'orange';
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(pointPI), center + radius*Math.sin(pointPI));
-    context.lineTo(center + radius*Math.cos(2*pointPI), center + radius*Math.sin(2*pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(3*pointPI), center + radius*Math.sin(3*pointPI));
-    context.lineTo(center + radius*Math.cos(4*pointPI), center + radius*Math.sin(4*pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(center + radius*Math.cos(5*pointPI), center + radius*Math.sin(5*pointPI));
-    context.lineTo(center + radius*Math.cos(6*pointPI), center + radius*Math.sin(6*pointPI));
-    context.lineTo(center, center);
-    context.fill();
-
-  }
-
   render() {
     return (
       <div>
-        <canvas ref="canvas" width={400} height={400} />
+        <canvas id="canvas" width={2*radius} height={2*radius} />
       </div>
     )
   }
