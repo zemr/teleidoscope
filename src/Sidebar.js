143502a99a76f19919f74e31fcca7e1db32d6a20
zemr <rzedzicka@gmail.com>
2017-04-04 23:04:02 +0200

    remove simple figures

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,24 +4,6 @@ import { Link } from 'react-router';
 const Sidebar = () => (
   <div className="sidebar">
     <div>
-      <Link to="/rectangle">
-        rectangle
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/triangle">
-        triangle
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/circle">
-        circle
-      </Link>
-    </div>
-
-    <div>
       <Link to="/hexagon">
         hexagon
       </Link>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
-import Rectangle from './figures/rectangle';
-import Circle from './figures/circle';
-import Triangle from './figures/triangle';
 import Hexagon from './figures/hexagon';
 import Hexagon6p from './figures/hexagon-6p';
 import Hexagon12p from './figures/hexagon-12p';
@@ -17,9 +14,6 @@ ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="rectangle" component={Rectangle}/>
-        <Route path="triangle" component={Triangle}/>
-        <Route path="circle" component={Circle}/>
         <Route path="hexagon" component={Hexagon}/>
         <Route path="hexagon-6p" component={Hexagon6p}/>
         <Route path="hexagon-12p" component={Hexagon12p}/>
