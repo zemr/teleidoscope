91830dffd59d0434356da73a407d73cbe87e1a92
zemr <rzedzicka@gmail.com>
2017-04-05 21:56:56 +0200

    refactor mirror/rotate fn

--- src/figures/hexagon-6sm.js
+++ src/figures/hexagon-6sm.js
@@ -3,6 +3,7 @@ import pattern1 from '../patterns/pattern1a.png';
 import pattern2 from '../patterns/pattern1b.png';
 
 const radius = 150, halfRadius = radius / 2;
+let i;
 
 class Hexagon extends React.Component {
   componentDidMount() {
@@ -22,7 +23,7 @@ class Hexagon extends React.Component {
       ctx.rotate(0);
       ctx.drawImage(img, -halfRadius, 0);
       // rotate by 60 deg (60*Math.PI/180)
-      for (let i = 0; i < 5; i++) {
+      for (i = 0; i < 5; i++) {
         ctx.rotate(Math.PI/3);
         ctx.drawImage(img, -halfRadius, 0);
       }
@@ -38,27 +39,24 @@ class Hexagon extends React.Component {
     img.src = file[source];
 
     img.onload = () => {
-      ctx.rotate(Math.PI/-3);
-      ctx.drawImage(img, -halfRadius, 0);
+      // positive, start at nine o'clock
+      for (i = 0; i < 3; i++) {
+        ctx.rotate(Math.PI/1.5);
+        ctx.drawImage(img, -halfRadius, 0);
+      }
+      // negative, start at seven o'clock
       ctx.rotate(Math.PI/3);
       ctx.save();
       ctx.scale(-1, 1);
       ctx.drawImage(img, -halfRadius, 0);
       ctx.restore();
-      ctx.rotate(Math.PI/3);
-      ctx.drawImage(img, -halfRadius, 0);
-      ctx.rotate(Math.PI/3);
-      ctx.save();
-      ctx.scale(-1, 1);
-      ctx.drawImage(img, -halfRadius, 0);
-      ctx.restore();
-      ctx.rotate(Math.PI/3);
-      ctx.drawImage(img, -halfRadius, 0);
-      ctx.rotate(Math.PI/3);
-      ctx.save();
-      ctx.scale(-1, 1);
-      ctx.drawImage(img, -halfRadius, 0);
-      ctx.restore();
+      for (i = 0; i < 2; i++) {
+        ctx.rotate(Math.PI/1.5);
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.drawImage(img, -halfRadius, 0);
+        ctx.restore();
+      }
     };
   }
 
@@ -78,7 +76,7 @@ class Hexagon extends React.Component {
         <canvas id="canvas3" width={2*radius+10} height={2*radius} />
         <canvas id="canvas4" width={2*radius} height={2*radius} />
        </div>
-    )
+    );
   }
 }
 
