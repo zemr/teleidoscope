88a2c53e4b338bef442e41dab85de52f6701c567
zemr <rzedzicka@gmail.com>
2017-04-06 14:03:53 +0200

    clone composite image

--- src/figures/hexagon-6C.js
+++ src/figures/hexagon-6C.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import pattern from '../patterns/test-pattern2.png';
 
+const radius = 200, height = 346, halfRadius = radius/2;
+
 class Hexagon extends React.Component {
   componentDidMount() {
     this.draw();
@@ -13,21 +15,28 @@ class Hexagon extends React.Component {
     const canvas2 = document.getElementById('canvas2');
     const context2 = canvas2.getContext('2d');
 
+    context.translate(radius, height/2);
+
     const imageObj = document.createElement('img');
     const file = {pattern};
     imageObj.src = file['pattern'];
 
     imageObj.onload = () => {
-      context.drawImage(imageObj, 0, 0);
-      context2.drawImage(canvas, 20, 20, 90, 90);
+      context.rotate(0);
+      context.drawImage(imageObj, -halfRadius, 0);
+      for (let i = 0; i < 5; i++) {
+        context.rotate(Math.PI/3);
+        context.drawImage(imageObj, -halfRadius, 0);
+      }
+      context2.drawImage(canvas, 50, 50, 150, 130);
     };
   }
 
   render() {
     return (
       <div>
-        <canvas id="canvas1" width="200" height="200" />
-        <canvas id="canvas2" width="200" height="200" />
+        <canvas id="canvas1" width={2*radius} height={height} />
+        <canvas id="canvas2" width={2*radius} height={height} />
       </div>
     );
   }
