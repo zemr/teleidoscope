5b03c38a578f0d21d13e0af63f51f94ec5971842
zemr <rzedzicka@gmail.com>
2017-04-08 22:36:23 +0200

    rearrange SC 6/v2

--- src/figures/hexagon-6F.js
+++ src/figures/hexagon-6F.js
@@ -25,74 +25,72 @@ class Hexagon extends React.Component {
       ctx.drawImage(img, 3.5*radius, .5*height);
       ctx.drawImage(img, 3.5*radius, 1.5*height);
 
-      ctx.translate(radius, height/2);
+      ctx.translate(radius, .5*height);
       ctx.rotate(Math.PI/1.5);
-      ctx.drawImage(img, -(.5*radius), 0*height); // 9
-      ctx.drawImage(img, -(.5*radius), -(1*height));
-      ctx.drawImage(img, -(.5*radius), -(2*height));
-      ctx.drawImage(img, -(.5*radius), -(3*height));
-      ctx.drawImage(img, -(2*radius), -(1.5*height));
       ctx.drawImage(img, -(2*radius), -(2.5*height));
-      ctx.drawImage(img, 1*radius, -(.5*height));
-      ctx.drawImage(img, 1*radius, -(1.5*height));
+      ctx.drawImage(img, -(2*radius), -(1.5*height));
+      ctx.drawImage(img, -(.5*radius), -(3*height));
+      ctx.drawImage(img, -(.5*radius), -(2*height));
+      ctx.drawImage(img, -(.5*radius), -(1*height));
+      ctx.drawImage(img, -(.5*radius), 0*height); // 9
       ctx.drawImage(img, 1*radius, -(2.5*height));
+      ctx.drawImage(img, 1*radius, -(1.5*height));
+      ctx.drawImage(img, 1*radius, -(.5*height));
       ctx.drawImage(img, 2.5*radius, -(1*height));
 
       ctx.rotate(Math.PI/1.5);
-      ctx.drawImage(img, -(.5*radius), 0*height); // 1
       ctx.drawImage(img, -(.5*radius), -(1*height));
+      ctx.drawImage(img, -(.5*radius), 0*height); // 1
+      ctx.drawImage(img, -(2*radius), -(1.5*height));
+      ctx.drawImage(img, -(2*radius), -(.5*height));
       ctx.drawImage(img, -(2*radius), .5*height);
       ctx.drawImage(img, -(2*radius), 1.5*height);
-      ctx.drawImage(img, -(2*radius), -(.5*height));
-      ctx.drawImage(img, -(2*radius), -(1.5*height));
-      ctx.drawImage(img, -(3.5*radius), 1*height);
-      ctx.drawImage(img, -(3.5*radius), 0*height);
       ctx.drawImage(img, -(3.5*radius), -(1*height));
+      ctx.drawImage(img, -(3.5*radius), 0*height);
+      ctx.drawImage(img, -(3.5*radius), 1*height);
       ctx.drawImage(img, -(5*radius), .5*height);
-
 
       ctx.rotate(Math.PI/3);
       ctx.save();
       ctx.scale(-1, 1);
-      ctx.drawImage(img, -(.5*radius), 0*height); // 3
-      ctx.drawImage(img, -(.5*radius), -(1*height));
-      ctx.drawImage(img, -(.5*radius), 1*height);
-      ctx.drawImage(img, -(.5*radius), 2*height);
       ctx.drawImage(img, -(2*radius), .5*height);
       ctx.drawImage(img, -(2*radius), 1.5*height);
+      ctx.drawImage(img, -(.5*radius), -(1*height));
+      ctx.drawImage(img, -(.5*radius), 0*height); // 3
+      ctx.drawImage(img, -(.5*radius), 1*height);
+      ctx.drawImage(img, -(.5*radius), 2*height);
       ctx.drawImage(img, 1*radius, -(.5*height));
       ctx.drawImage(img, 1*radius, .5*height);
       ctx.drawImage(img, 1*radius, 1.5*height);
       ctx.drawImage(img, 2.5*radius, 0*height);
       ctx.restore();
 
-
       ctx.rotate(Math.PI/1.5);
       ctx.save();
       ctx.scale(-1, 1);
-      ctx.drawImage(img, -(.5*radius), 0*height); // 7
       ctx.drawImage(img, -(.5*radius), -(1*height));
-      ctx.drawImage(img, -(2*radius), .5*height);
-      ctx.drawImage(img, -(2*radius), -(.5*height));
-      ctx.drawImage(img, -(2*radius), -(1.5*height));
+      ctx.drawImage(img, -(.5*radius), 0*height); // 7
       ctx.drawImage(img, -(2*radius), -(2.5*height));
-      ctx.drawImage(img, -(3.5*radius), 0*height);
-      ctx.drawImage(img, -(3.5*radius), -(1*height));
+      ctx.drawImage(img, -(2*radius), -(1.5*height));
+      ctx.drawImage(img, -(2*radius), -(.5*height));
+      ctx.drawImage(img, -(2*radius), .5*height);
       ctx.drawImage(img, -(3.5*radius), -(2*height));
+      ctx.drawImage(img, -(3.5*radius), -(1*height));
+      ctx.drawImage(img, -(3.5*radius), 0*height);
       ctx.drawImage(img, -(5*radius), -(1.5*height));
       ctx.restore();
 
       ctx.rotate(Math.PI/1.5);
       ctx.save();
       ctx.scale(-1, 1);
-      ctx.drawImage(img, -(.5*radius), 0*height); // 11
-      ctx.drawImage(img, -(.5*radius), -(1*height));
       ctx.drawImage(img, -(.5*radius), -(2*height));
-      ctx.drawImage(img, 1*radius, -(.5*height));
+      ctx.drawImage(img, -(.5*radius), -(1*height));
+      ctx.drawImage(img, -(.5*radius), 0*height); // 11
       ctx.drawImage(img, 1*radius, -(1.5*height));
-      ctx.drawImage(img, 2.5*radius, 0*height);
-      ctx.drawImage(img, 2.5*radius, -(1*height));
+      ctx.drawImage(img, 1*radius, -(.5*height));
       ctx.drawImage(img, 2.5*radius, -(2*height));
+      ctx.drawImage(img, 2.5*radius, -(1*height));
+      ctx.drawImage(img, 2.5*radius, 0*height);
       ctx.restore();
 
     };
