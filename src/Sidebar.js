6f58cbc5da652aff997fde28c598005df3035b74
zemr <rzedzicka@gmail.com>
2017-04-08 23:11:25 +0200

    remove working files

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,56 +4,8 @@ import { Link } from 'react-router';
 const Sidebar = () => (
   <div className="sidebar">
     <div>
-      <Link to="/hexagon">
-        hexagon
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6p">
-        hexagon 6p
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-12p">
-        hexagon 12p
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6L">
-        hexagon 6L
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6M">
-        hexagon 6M
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6s">
-        hexagon 6s
-      </Link>
-    </div>
-
-    <div>
       <Link to="/hexagon-6sm">
         hexagon 6sm
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6smm">
-        hexagon 6smm
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6C">
-        hexagon 6C
       </Link>
     </div>
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
-import Hexagon from './figures/hexagon';
-import Hexagon6p from './figures/hexagon-6p';
-import Hexagon12p from './figures/hexagon-12p';
-import Hexagon6L from './figures/hexagon-6L';
-import Hexagon6M from './figures/hexagon-6M';
-import Hexagon6s from './figures/hexagon-6s';
 import Hexagon6sm from './figures/hexagon-6sm';
-import Hexagon6smm from './figures/hexagon-6smm';
-import Hexagon6C from './figures/hexagon-6C';
 import Hexagon6CM from './figures/hexagon-6CM';
 import Hexagon6F from './figures/hexagon-6F';
 
@@ -20,15 +12,7 @@ ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="hexagon" component={Hexagon}/>
-        <Route path="hexagon-6p" component={Hexagon6p}/>
-        <Route path="hexagon-12p" component={Hexagon12p}/>
-        <Route path="hexagon-6L" component={Hexagon6L}/>
-        <Route path="hexagon-6M" component={Hexagon6M}/>
-        <Route path="hexagon-6s" component={Hexagon6s}/>
         <Route path="hexagon-6sm" component={Hexagon6sm}/>
-        <Route path="hexagon-6smm" component={Hexagon6smm}/>
-        <Route path="hexagon-6C" component={Hexagon6C}/>
         <Route path="hexagon-6CM" component={Hexagon6CM}/>
         <Route path="hexagon-6F" component={Hexagon6F}/>
       </Route>
