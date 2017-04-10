658a905895e0ee32ed4250d8e0a7f893a0405362
zemr <rzedzicka@gmail.com>
2017-04-10 15:53:01 +0200

    repeatedly change pattern

--- src/scope/ScopeButton.js
+++ src/scope/ScopeButton.js
@@ -3,13 +3,27 @@ import React from 'react';
 const patterns = [
   {source: process.env.PUBLIC_URL + '/cells/cell_1_1.png'},
   {source: process.env.PUBLIC_URL + '/cells/cell_1_2.png'},
-  {source: process.env.PUBLIC_URL + '/cells/cell_2_3.png'},
-  {source: process.env.PUBLIC_URL + '/cells/cell_2_4.png'}
+  {source: process.env.PUBLIC_URL + '/cells/cell_1_3.png'},
+  {source: process.env.PUBLIC_URL + '/cells/cell_1_4.png'}
 ];
+
+const cells = (activeCell) => {
+  return (
+    patterns.filter(pattern =>
+      pattern.source.split('_')[1] === activeCell.split('_')[1]
+    ).filter(pattern =>
+      pattern.source.split('_')[2] !== activeCell.split('_')[2]
+    )
+  );
+};
+
+const newCell = (set) => {
+  return set[Math.floor(Math.random()*set.length)];
+};
 
 const ScopeButton = (props) => {
   return (
-    <button onClick={ () => props.changePattern(patterns[1].source) }>
+    <button onClick={ () => props.changePattern(newCell(cells(props.pattern)).source) }>
       move
     </button>
   );
