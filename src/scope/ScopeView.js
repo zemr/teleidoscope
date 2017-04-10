c2a302238b815566b883f5c32496a67d0f6554b9
zemr <rzedzicka@gmail.com>
2017-04-10 19:17:56 +0200

    responsive switch between views

--- src/scope/ScopeView.js
+++ src/scope/ScopeView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Scope from './Scope';
+import ScopeMobile from './ScopeMobile';
 import ScopeButton from './ScopeButton';
 
 class ScopeView extends React.Component {
@@ -7,26 +8,42 @@ class ScopeView extends React.Component {
     super(props);
 
     this.state = {
+      screen: screen.width,
+      window: window.innerWidth,
       pattern: process.env.PUBLIC_URL + '/cells/cell_1_1.png'
     };
 
     this.changePattern = this.changePattern.bind(this);
+    this.updateWidth = this.updateWidth.bind(this);
   }
 
   changePattern(newPattern) {
     this.setState({pattern: newPattern});
   }
 
+  updateWidth() {
+    this.setState({screen: screen.width, window: window.innerWidth});
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateWidth);
+  }
+
   render() {
     return (
       <div>
         <ScopeButton pattern={this.state.pattern} changePattern={this.changePattern} />
-        <Scope pattern={this.state.pattern}/>
+        {
+          this.state.screen > 993 && this.state.window > 993
+            ?
+            <Scope pattern={this.state.pattern} />
+            :
+            <ScopeMobile pattern={this.state.pattern} />
+        }
         {console.log(this.state.pattern)}
       </div>
     );
   }
 }
-
 
 export default ScopeView;
