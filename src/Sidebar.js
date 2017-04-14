a8735bcb1c4dc494f1213192bfb4e9babb832e4e
zemr <rzedzicka@gmail.com>
2017-04-14 13:11:31 +0200

    remove working files (hexagon)

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,30 +4,6 @@ import { Link } from 'react-router';
 const Sidebar = () => (
   <div className="sidebar">
     <div>
-      <Link to="/hexagon-6sm">
-        hexagon 6sm
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6CM">
-        hexagon 6CM
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6F">
-        hexagon 6F
-      </Link>
-    </div>
-
-    <div>
-      <Link to="/hexagon-6G">
-        hexagon 6G
-      </Link>
-    </div>
-
-    <div>
       <Link to="/scope">
         scope
       </Link>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
-import Hexagon6sm from './figures/hexagon-6sm';
-import Hexagon6CM from './figures/hexagon-6CM';
-import Hexagon6F from './figures/hexagon-6F';
-import Hexagon6G from './figures/hexagon-6G';
 import ScopeView from './scope/ScopeView';
 import PatternView from './pattern/PatternView';
 
@@ -15,10 +11,6 @@ ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="hexagon-6sm" component={Hexagon6sm}/>
-        <Route path="hexagon-6CM" component={Hexagon6CM}/>
-        <Route path="hexagon-6F" component={Hexagon6F}/>
-        <Route path="hexagon-6G" component={Hexagon6G}/>
         <Route path="scope" component={ScopeView}/>
         <Route path="scope/:patternId" component={ScopeView}/>
         <Route path="pattern" component={PatternView}/>
