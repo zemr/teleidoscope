9a474ab4947666c02624ac8b6a1ddc4759cda638
zemr <rzedzicka@gmail.com>
2017-04-22 18:27:28 +0200

    credit images

--- src/pattern/PatternView.js
+++ src/pattern/PatternView.js
@@ -5,23 +5,44 @@ const PatternView = () => {
   return (
     <div className="patterns-container">
       <Link to="/scope/1">
-        <img src={process.env.PUBLIC_URL + '/patterns/pattern_1.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_1.png'} alt="pattern1" />
+          <span className="pattern-source">Vecteezy.com</span>
+        </div>
       </Link>
       <Link to="/scope/2">
-        <img src={process.env.PUBLIC_URL + '/patterns/pattern_2.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_2.png'} alt="pattern2" />
+          <span className="pattern-source">PeHaa.com</span>
+        </div>
       </Link>
       <Link to="/scope/3">
-       <img src={process.env.PUBLIC_URL + '/patterns/pattern_3.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_3.png'} alt="pattern3" />
+          <span className="pattern-source">PeHaa.com</span>
+        </div>
       </Link>
       <Link to="/scope/4">
-        <img src={process.env.PUBLIC_URL + '/patterns/pattern_4.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_4.png'} alt="pattern4" />
+          <span className="pattern-source">Vecteezy.com</span>
+        </div>
       </Link>
       <Link to="/scope/5">
-      <img src={process.env.PUBLIC_URL + '/patterns/pattern_5.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_5.png'} alt="pattern5" />
+          <span className="pattern-source">Vecteezy.com</span>
+        </div>
       </Link>
       <Link to="/scope/6">
-      <img src={process.env.PUBLIC_URL + '/patterns/pattern_6.png'} alt="" className="pattern" />
+        <div className="pattern">
+          <img src={process.env.PUBLIC_URL + '/patterns/pattern_6.png'} alt="pattern6" />
+          <span className="pattern-source">Vecteezy.com</span>
+        </div>
       </Link>
+      <div className="sources">
+        Graphics by <a target="_blank" href="https://www.vecteezy.com">Vecteezy</a>, <a target="_blank" href="http://pehaa.com/">PeHaa</a>
+      </div>
     </div>
   );
 };
